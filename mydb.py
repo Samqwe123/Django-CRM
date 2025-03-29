26a83b52dc6d778a6156ca3649b18267ab155704
sam <khor760517@gmail.com>
2025-03-29 16:36:07 +0800

    Initial commit

---
+++ mydb.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+# Connect to MySQL server (without specifying a database)
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="admin"
+)
+
+# Prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a new database
+cursorObject.execute("CREATE DATABASE samdb")
+
+print("Database samdb has been recreated successfully!")
+
+# Close the connection
+cursorObject.close()
+dataBase.close()
